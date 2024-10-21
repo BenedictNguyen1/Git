fbc2bb41fde816a4cd3645ace37be8723322f151
BenedictNguyen <benedictnguyen@cu-biotedu-3-10.235.148.58.int.colorado.edu>
2024-10-20 19:17:01 -0600

    second commit

--- yo.cpp
+++ yo.cpp
@@ -2,7 +2,11 @@
 using namespace std;
 
 int main(){
-    cout << "Hello world" << endl;
+    
+    for (int i=0; i < 4; i++){
+        cout << "Hello world x" << i << endl;
+    }
+
 
     return 0;
 }
